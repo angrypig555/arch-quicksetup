a06220dd49701bb75445553bfc8191ea3e0d255f
angrypig555 <brny@brny.dev>
2026-07-19 15:23:23 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "arch vm autoinstall script"
+echo "only for amd64 UEFI vmware machines with the default config"
+echo "if you want to change the timezone or disk, feel free to modify the script"
+echo "this script is fully automatic and assumes you have an internet connection as you already downloaded this"
+echo "setup starting in 10 seconds"
+sleep 10
+# IF YOU CHANGE THIS YOU ALSO NEED TO CHANGE THE DISK IN stage2.sh WHERE THE BOOTLOADER CONFIG IS GENERATED
+DISK="/dev/sda"
+ESP="/dev/sda1"
+SWAP="/dev/sda2"
+ROOT="/dev/sda3"
+
+echo "partitioning disk"
+sfdisk "$DISK"<< EOF
+label: gpt
+,1GB,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+,2GB,0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+,,4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
+EOF
+
+echo "formatting disk"
+mkfs.ext4 "$ROOT"
+mkswap "$SWAP"
+mkfs.fat -F 32 "$ESP"
+
+echo "mounting disk"
+mount "$ROOT" /mnt
+mount --mkdir "$ESP" /mnt/boot
+swapon "$SWAP"
+
+echo "pacstrapping"
+pacstrap --noconfirm -K /mnt base linux linux-firmware nano vim sudo networkmanager ufw openssh
+echo "generating fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "starting stage 2"
+cp stage2.sh /mnt/root/
+chmod +x /mnt/root/stage2.sh
+
+arch-chroot /mnt /root/stage2.sh
+
+echo "installation finished"
+echo "goodbye!"
+umount -R /mnt
+reboot
---
+++ stage2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo "arch-quicksetup stage 2"
+echo "enabling networking"
+systemctl enable NetworkManager.service
+
+echo "setting timezone"
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+echo "setting system clock"
+hwclock --systohc
+echo "setting locale"
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "setting hostname"
+echo "arch-vm" > /etc/hostname
+echo "root password is root"
+echo  "root:root" | chpasswd
+useradd -m -G sudo user
+echo "created priviliged user without a password"
+echo "installing systemd-boot"
+bootctl install
+echo "creating boot entries and configs"
+cat << EOF > /boot/loader/loader.conf
+default arch.conf
+timeout 0
+editor no
+EOF
+# IF YOU CHANGED THE DISK VARIABLE IN THE FIRST SCRIPT YOU ALSO NEED TO CHANGE IT HERE
+cat << EOF > /boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=UUID=$(blkid -s UUID -o value /dev/sda1) rw
+EOF
+cat << EOF > /etc/motd
+If you want to disable this, delete /etc/motd
+!!! CHANGE THE ROOT PASSWORD IMMIDIAETLY !!!
+The root password is a default and is easily guessable.
+Also don't forget to set a password for the user account as you can't login yet.
+EOF
+echo "enabling firewall"
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow SSH
+sed -i 's/ENABLED=no/ENABLED=yes/' /etc/ufw/ufw.conf
+systemctl enable ufw.service
+echo "enabling ssh"
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl enable sshd.service
+echo "stage 2 complete, exiting..."
+exit
